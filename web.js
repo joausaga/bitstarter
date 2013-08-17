2992ef047c5ddcfc8e236d2218791ea340f1efc0
joausaga <jorgesaldivar@gmail.com>
2013-08-17 17:41:36 +0000

    Going back

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
+//var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buf = fs.readFileSync("index.html");
+//var buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send('Hello World2');
 });
 
 var port = process.env.PORT || 5000;
